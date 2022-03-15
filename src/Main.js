bce3e794d192c6cbe76c968e912b4519119bbeda
James Brooks <brooksjamesk01@gmail.com>
2022-03-14 21:24:02 -0700

    added hornedBeast component data

--- src/Main.js
+++ src/Main.js
@@ -5,14 +5,8 @@ class Main extends React.Component {
   render() {
     return (
         <main>
-          <HornedBeast 
-            name="Sheyna" 
-            hometown="Seattle" 
-            hairColor="Brown"
-          />
-          <HornedBeast name="Cole"/>
-          <HornedBeast name="Nathan"/>
-          <HornedBeast name="Eden"/>
+          <HornedBeast title="Goat" imageURL="./imgs/pexels-ekaterina-bolovtsova-5263829.jpg" description="A Goat" />
+          <HornedBeast title="Hebridean Sheep" imageURL="./imgs/Hebridean_Sheep.jpg" description="A Hebridian Sheep"/>
         </main>
     );
   }
