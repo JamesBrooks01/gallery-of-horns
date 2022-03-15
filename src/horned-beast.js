80058bf93a05fc03a8dbddbd0d81cbbbe4525c4b
James Brooks <brooksjamesk01@gmail.com>
2022-03-14 20:56:04 -0700

    added horned-beast component

---
+++ src/horned-beast.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+  render() {
+    return (
+      <article>
+       <h2>{this.props.title}</h2>
+       <img src={require(`${this.props.imageURL}`)} alt={`${this.props.title}`} title={`${this.props.title}`}/>
+       <p>{this.props.description}</p>
+      </article>
+    );
+  }
+}
+
+export default HornedBeast;
