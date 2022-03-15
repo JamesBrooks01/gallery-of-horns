1cae1c47f8e8c2c152b0c21fc862fbd49d499c71
James Brooks <brooksjamesk01@gmail.com>
2022-03-15 16:36:53 -0700

    added in features to component

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: 0,
+    }
+  }
+  handleFavs = () => {
+    this.setState({
+      favorites: this.state.favorites + 1
+    });
+  };
   render() {
     return (
       <article>
        <h2>{this.props.title}</h2>
-       <img src={require(`${this.props.imageURL}`)} alt={`${this.props.title}`} title={`${this.props.title}`}/>
+       <img src={this.props.image_url} alt={this.props.title} title={this.props.title} onClick={this.handleFavs}/>
+       <p>{this.state.favorites}❤️</p>
        <p>{this.props.description}</p>
       </article>
     );
