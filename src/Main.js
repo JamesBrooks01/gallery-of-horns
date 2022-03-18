e25850a26181173703b46e1a704c50d7bda689d9
James Brooks <brooksjamesk01@gmail.com>
2022-03-17 19:12:14 -0700

    added code for Form Input handling for filtering images by numHorns

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
+import FormComponent from './Form.js';
 import './Main.css';
 
 class Main extends React.Component {
@@ -7,21 +8,33 @@ class Main extends React.Component {
     super(props);
     this.state = {
       data: this.props.data,
+      allBeasts: this.props.data
     }
   }
+handleFormInput = (numHorns) => {
+  console.log(numHorns)
+  let newBeasts = (this.props.data).filter(i => i.horns === +numHorns);
+  console.log(newBeasts);
+  console.log(this.props.data[0].horns)
+  this.setState({ allBeasts: newBeasts });
+}
+
   render() {
+    let allBeasts = this.state.allBeasts.map((beast, i) => (
+      <HornedBeast
+        title={beast.title}
+        image_url={beast.image_url}
+        description={beast.description}
+        horns={beast.horns}
+        key={i}
+        displayModal={this.props.displayModal}
+      />
+    ))
     return (
       <main>
-          {this.state.data.map((beast, i) => (
-              <HornedBeast
-                title={beast.title}
-                image_url={beast.image_url}
-                description={beast.description}
-                key={i}
-                displayModal={this.props.displayModal}
-              />
-          ))}
-        </main>
+        <FormComponent handleFormInput={this.handleFormInput} className="formCSS"/>
+        {allBeasts}
+      </main>
     );
   }
 }
